058b23f2e8cb0094cb20ff30f9306308c00c9d6d
Philip Wood <philip.m.wood@gmail.com>
2010-04-04 12:45:51 +0000

    Issue 3: All dlls use shared versioning

--- Development/Bluejam.Utils.DatabaseScripter.DbAdapter/Properties/AssemblyInfo.cs
+++ Development/Bluejam.Utils.DatabaseScripter.DbAdapter/Properties/AssemblyInfo.cs
@@ -24,16 +24,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4ccbdc56-6bb6-45c1-a06e-ba9d856331fb")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
