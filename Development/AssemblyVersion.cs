f2bc389c15b8b6b55f39ad092a9d316204897730
Philip Wood <philip.m.wood@gmail.com>
2010-08-05 21:07:36 +0000

    #11 Attempt to fix the build

--- Development/AssemblyVersion.cs
+++ Development/AssemblyVersion.cs
@@ -10,6 +10,6 @@ using System.Reflection;
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: AssemblyVersionAttribute("0.9.0.212")]
-[assembly: AssemblyFileVersionAttribute("0.9.0.212")]
+[assembly: AssemblyVersionAttribute("0.9.0.214")]
+[assembly: AssemblyFileVersionAttribute("0.9.0.214")]
 
